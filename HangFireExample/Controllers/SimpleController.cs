9fe5ff8a91d4dffe62ff5d7846110d3bcf817fc9
Hasan Akpolad <hasan.akpolad@detaysoft.com>
2022-07-07 10:50:47 +0300

    added jobs

---
+++ HangFireExample/Controllers/SimpleController.cs
@@ -0,0 +1,16 @@
+﻿using HangFireExample.JobsManagers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFireExample.Controllers
+{
+    [Route("[controller]")]
+    public class SimpleController : ControllerBase
+    {
+        [HttpGet(nameof(GetResult))]
+        public IActionResult GetResult()
+        {
+            JobsManager.Instance.StartReccuringJob();
+            return Ok("deneme");
+        }
+    }
+}
---
+++ HangFireExample/JobsManagers/JobsManager.cs
@@ -0,0 +1,44 @@
+﻿using Hangfire;
+
+namespace HangFireExample.JobsManagers
+{
+    public class JobsManager
+    {
+        #region Singleton
+
+        private static Lazy<JobsManager> instance = new Lazy<JobsManager>(() => new JobsManager());
+        public static JobsManager Instance => instance.Value;
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Methods
+
+        public void StartReccuringJob()
+        {
+            RecurringJob.AddOrUpdate("Recurring Jobs", () => Console.WriteLine("Recurring Jobs"), Cron.Hourly);
+        }
+
+        public void StartFireForgetJob()
+        {
+            var jobId = BackgroundJob.Enqueue(() => Console.WriteLine("Fire and Forget Jobs"));
+        }
+
+        public void StartDelayedJob()
+        {
+            var jobId = BackgroundJob.Schedule(() => Console.WriteLine("Delayed Jobs"), TimeSpan.FromMinutes(10));
+        }
+
+        //public void StartContinuationsJob()
+        //{
+        //    var jobId = 1;
+        //    return BackgroundJob.ContinueJobWith(jobId, () => Console.WriteLine("Continuation!"));
+        //}
+        #endregion
+    }
+}

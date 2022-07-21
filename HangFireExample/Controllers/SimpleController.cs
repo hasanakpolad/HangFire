866b4517157fca2f57afbaca425df9573e7ee18b
Hasan Akpolad <hasan.akpolad@detaysoft.com>
2022-07-21 18:03:07 +0300

    docker file fixes

--- HangFireExample/Controllers/SimpleController.cs
+++ HangFireExample/Controllers/SimpleController.cs
@@ -1,16 +1,23 @@
 ﻿using HangFireExample.JobsManagers;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 namespace HangFireExample.Controllers
 {
     [Route("[controller]")]
     public class SimpleController : ControllerBase
     {
+        private static List<byte[]> _data = new List<byte[]>();
+
         [HttpGet(nameof(GetResult))]
         public IActionResult GetResult()
         {
-            JobsManager.Instance.StartReccuringJob();
-            return Ok("deneme");
+            var bytes = new byte[1024 * 1024 * 100];
+            var random = RandomNumberGenerator.Create();
+            random.GetBytes(bytes);
+            _data.Add(bytes);
+            JobsManager.Instance.StartFireForgetJob();
+            return Ok("Job has Started");
         }
     }
 }

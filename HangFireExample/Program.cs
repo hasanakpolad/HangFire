f931dafb3f721c93bd9ff5878c71c28c214505f3
Hasan Akpolad <hasan.akpolad@detaysoft.com>
2022-07-22 09:36:15 +0300

    some fix

--- HangFireExample/Program.cs
+++ HangFireExample/Program.cs
@@ -3,9 +3,9 @@ using Hangfire.MySql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.
 // Add services to the container.
 
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddHangfire(conf =>
 {
